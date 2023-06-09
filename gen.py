17d12724d4cd47ca78ad320bdec7d38ad4ca5d9f
KtrauM <murat.kurnaz12@gmail.com>
2023-06-09 06:10:55 +0200

    Add testing for linear rmq

--- gen.py
+++ gen.py
@@ -1,8 +1,9 @@
 import random
-N = 1500
+N = 300
 print(N)
 for i in range(N):
-    print(random.randint(-9223372036854775808, 9223372036854775807))
+    print(random.randint(-50,50))
+    # print(random.randint(-9223372036854775808, 9223372036854775807))
 for i in range(N):
     for j in range(i, N):
         print(f'{i},{j}')
---
+++ linear-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Run the commands and generate diff files
+run_ads_programm() {
+    # Remove any existing output files
+    rm -f diff*.txt
+    
+    differences_found=false
+    
+    for ((i=1; i<=200; i++))
+    do
+        echo "Iteration $i"
+        
+        python3 gen.py > input.txt
+
+        # Run the first command
+        ./ads_programm rmq input.txt linear.txt
+        
+        # Run the second command
+        ./ads_programm rmq input.txt naive.txt
+        
+        # Generate diff file between sparse.txt and naive.txt
+        diff_output=$(diff linear.txt naive.txt)
+        
+        if [[ -n "$diff_output" ]]; then
+            differences_found=true
+            echo "Difference found in iteration $i"
+            break
+        fi
+        
+        echo "$diff_output" > "diff${i}.txt"
+    done
+    
+    if [[ $differences_found = true ]]; then
+        echo "Differences found in at least one iteration."
+    else
+        echo "No differences found in any iteration."
+    fi
+}
+
+# Call the function to execute the commands
+run_ads_programm
