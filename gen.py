d1b5259706a4ffadb6899047a03386db4dab3041
KtrauM <murat.kurnaz12@gmail.com>
2023-07-17 06:31:22 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,9 +1,16 @@
 import random
-N = 300
+N = 10
 print(N)
 for i in range(N):
-    print(random.randint(-50,50))
-    # print(random.randint(-9223372036854775808, 9223372036854775807))
+    # print(random.randint(-50,50))
+    print(random.randint(-9223372036854775808, 9223372036854775807))
+# for i in range(N):
+#     for j in range(i, N):
+#         print(f'{i},{j}')
+
 for i in range(N):
-    for j in range(i, N):
-        print(f'{i},{j}')
+    k = random.randint(0, N)
+    l = random.randint(0, N)
+    if k > l:
+        k, l = l, k
+    print(f'{k},{l}')
